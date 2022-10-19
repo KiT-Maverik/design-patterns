f570f3fb7eaf6afbb27fea73c747ba1fafca506a
kit <kit.mavareik@gmail.com>
2022-10-19 17:30:26 +0300

    Introducing new logic

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import {calculateTotal} from "./utils/order.utils.js";
+
 const response = await fetch('http://localhost:3000/api/orders')
 const json = await response.json()
-console.log(json)
+console.log(json.map(order => calculateTotal(order)));
