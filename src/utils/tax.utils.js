6a102ee3705b9b644bb966881941bdda132da472
kit <kit.mavareik@gmail.com>
2022-10-19 17:29:36 +0300

    Fixing unexpected strings

--- src/utils/tax.utils.js
+++ src/utils/tax.utils.js
@@ -3,7 +3,7 @@ import {TAX} from "../constants/tax.constants.js";
 function calculateTax(price, country) {
     // if wrong country
 
-    return (price / 100 * TAX[country]).toFixed(2)
+    return parseFloat((price / 100 * TAX[country]).toFixed(2));
 }
 
 export {calculateTax};
