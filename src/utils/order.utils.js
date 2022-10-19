c03bfc37158ff44187e029463e2e4ea0feaf1da0
kit <kit.mavareik@gmail.com>
2022-10-19 16:12:40 +0300

    Implementing calculateTotal util

--- src/utils/order.utils.js
+++ src/utils/order.utils.js
@@ -1,28 +1,51 @@
 import {DISABLED, STANDARD} from "../constants/reduction.constants.js";
+import {calculateTax} from "./tax.utils.js";
 
-export function calculateSubtotal(price, qty) {
+function calculateSubtotal(prices, qty) {
     // non equal lists
     let subtotal = 0;
 
-    for (let i = price.length; i--; i) {
-        subtotal += price[i] * qty[i];
+    for (let i = prices.length; i--; i) {
+        subtotal += prices[i] * qty[i];
     }
 
     return subtotal;
 }
 
-export function getReductionScheme(scheme) {
+function getReductionScheme(scheme) {
     switch (scheme) {
         case "STANDARD" : return STANDARD;
         default: return DISABLED;
     }
 }
 
-export function calculateReduction(price, scheme) {
-    return scheme.reduce((acc, {discount, threshold}) => {
+function calculateDiscount(price, scheme) {
+    const rate = scheme.reduce((acc, {discount, threshold}) => {
         if (price >= threshold && discount > acc) return discount;
 
         return acc
-    }, 0)
+    }, 0);
+
+    const amount = rate / price * 100;
+
+    return {
+        rate,
+        amount,
+    }
 }
+
+function calculateTotal({country, prices, quantities, reduction}) {
+    const subtotal = calculateSubtotal(prices, quantities);
+    const tax = calculateTax(subtotal, country);
+    const discount = calculateDiscount(subtotal + tax, getReductionScheme(reduction));
+
+    return {
+        discount,
+        subtotal,
+        tax,
+        total: subtotal + tax - discount.amount,
+    };
+}
+
+export {calculateDiscount, calculateSubtotal, calculateTotal}
 
