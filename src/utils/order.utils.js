d4ae11110731bbe40ba4e6d629d955ebcba969a7
kit <kit.mavareik@gmail.com>
2022-10-19 15:40:44 +0300

    implementing getReductionScheme && calculateReduction utils

--- src/utils/order.utils.js
+++ src/utils/order.utils.js
@@ -1,3 +1,5 @@
+import {DISABLED, STANDARD} from "../constants/reduction.constants.js";
+
 export function calculateSubtotal(price, qty) {
     // non equal lists
     let subtotal = 0;
@@ -8,3 +10,19 @@ export function calculateSubtotal(price, qty) {
 
     return subtotal;
 }
+
+export function getReductionScheme(scheme) {
+    switch (scheme) {
+        case "STANDARD" : return STANDARD;
+        default: return DISABLED;
+    }
+}
+
+export function calculateReduction(price, scheme) {
+    return scheme.reduce((acc, {discount, threshold}) => {
+        if (price >= threshold && discount > acc) return discount;
+
+        return acc
+    }, 0)
+}
+
