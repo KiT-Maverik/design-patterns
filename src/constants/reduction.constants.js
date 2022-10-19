79e9761296e86558a132f64fef79be814b386473
kit <kit.mavareik@gmail.com>
2022-10-19 15:40:57 +0300

    Introducing reduction schemes

--- src/constants/reduction.constants.js
+++ src/constants/reduction.constants.js
@@ -1,5 +1,31 @@
-const STANDARD = {
+const DISABLED = [
+    {
+        threshold: 0,
+        discount: 0,
+    }
+];
 
-};
+const STANDARD = [
+    {
+        threshold: 1000,
+        discount: 3,
+    },
+    {
+        threshold: 5000,
+        discount: 5,
+    },
+    {
+        threshold: 7000,
+        discount: 7,
+    },
+    {
+        threshold: 10000,
+        discount: 10,
+    },
+    {
+        threshold: 50000,
+        discount: 15,
+    },
+];
 
-export {STANDARD};
+export {DISABLED, STANDARD};
