69c84d35e0d978de247b55fd9b3f13717cbe0e01
kit <kit.mavareik@gmail.com>
2022-11-10 10:41:59 +0200

    Misconception fix

--- structural/decorator.ts
+++ structural/decorator.ts
@@ -16,22 +16,22 @@ class Circle {
     }
 }
 
-class MetricAdapter implements ICircleApi {
+class MetricDecorator implements ICircleApi {
     private coefficient = 2.5;
-    private adaptee: ICircleApi;
+    private wrappee: ICircleApi;
 
-    constructor(adaptee: ICircleApi) {
-        this.adaptee = adaptee;
+    constructor(wrappee: ICircleApi) {
+        this.wrappee = wrappee;
     }
 
     getRadius() {
         // Wa-la - inches converted to centimeters
-        return this.adaptee.getRadius() * this.coefficient;
+        return this.wrappee.getRadius() * this.coefficient;
     }
 }
 
 // PROOF OF CONCEPT
-const MetricAPI = new MetricAdapter(new CircleAPI);
+const MetricAPI = new MetricDecorator(new CircleAPI);
 Circle.draw(MetricAPI.getRadius());
 
 // TODO: validate
